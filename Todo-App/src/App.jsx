800f8513851677fe38c78d2b8fc606306177a59d
BlesonPhilip <blesonphilip21@gmail.com>
2025-02-24 18:49:04 +0530

    Expand todo with modal

---
+++ Todo-App/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className=" todo-container">
+        <div className="banner">
+          <img src="/car.jpg" alt="car" />
+          <div className="bar-container">
+            <div className="bar"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
