f562885bc5a032ba6bc271a0cbb479bf1bb01128
BlesonPhilip <blesonphilip21@gmail.com>
2025-02-25 09:49:38 +0530

    modified app.jsx

--- Todo-App/src/App.jsx
+++ Todo-App/src/App.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Modal from "./components/modal";
 
 const App = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [todo, setTodo] = useState([]);
+  const [singleTodo, setSingleTodo] = useState({
+    titile: "",
+    description: "",
+    time: "",
+    priority: "",
+  });
+const onTitlechange=titile=>{
+setSingleTodo(...singleTodo,titile:titile)
+}
+
   return (
-    <>
-      <div className=" todo-container">
-        <div className="banner">
-          <img src="/car.jpg" alt="car" />
-          <div className="bar-container">
-            <div className="bar"></div>
+    <div className=" todo-container">
+      <div className="banner">
+        <img src="/car.jpg" alt="car" />
+        <div className="bar-container">
+          <div className="bar" onClick={() => setShowModal(true)}>
+            <p>Create Your Todo...</p>
           </div>
         </div>
       </div>
-    </>
+      <div className="todo-list">
+        {todo.map((item, index) => (
+          <div className="todo-list-item" key={index}>
+            <h4>{item.title}</h4>
+            <p>{item.description}</p>
+            <div className="time-priority">
+              <p>{item.time}</p>
+              <p>{item.priority}</p>
+            </div>
+            <div className="b">
+              <button>Update</button>
+              <button>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Modal show={showModal} setShow={setShowModal} onTitleChange={onTitleChange} />
+    </div>
   );
 };
 
--- Todo-App/src/components/modal.jsx
+++ Todo-App/src/components/modal.jsx
@@ -1,5 +1,30 @@
 import "./modal.css";
 
-const Modal = () => {
-  return <>.</>;
+const Modal = ({ show, setShow, onTitleChange }) => {
+  const onInputChange = (e) => {
+    onTitleChange(e.target.value);
+  };
+
+  return (
+    <div style={{ display: show ? "block" : "none" }}>
+      <div className="modal-background" onClick={() => setShow(false)}></div>
+      <div className="modal-container">
+        <div className="model">
+          <label>Title</label>
+          <input className="title" type="text" onChange={onInputChange} />
+          <label>Description</label>
+          <textarea className="description"></textarea>
+
+          <input type="time" className="time" />
+          <select name="" id="" className="priority">
+            <option value="">Low</option>
+            <option value="">Medium</option>
+            <option value="">High</option>
+          </select>
+          <button className="btn">Add</button>
+        </div>
+      </div>
+    </div>
+  );
 };
+export default Modal;
