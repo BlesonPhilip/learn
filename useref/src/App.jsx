6459ea32aa1419629d098f2f2df4f7b501a8604f
BlesonPhilip <blesonphilip21@gmail.com>
2025-03-01 09:35:12 +0530

    Pratice using useref and useState .

---
+++ useref/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="form">
+          <div className="input-div"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
