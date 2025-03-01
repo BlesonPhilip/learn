8d4cee07609118a19d2ea8b34c2f7555e5832e33
BlesonPhilip <blesonphilip21@gmail.com>
2025-03-01 09:44:51 +0530

    styled in app.css and build a input form in app.js

--- useref/src/App.jsx
+++ useref/src/App.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import "./App.css";
+import { useRef } from "react";
 
 const App = () => {
+  const nameRef = useRef();
   return (
     <>
       <div className="container">
         <div className="form">
-          <div className="input-div"></div>
+          <div className="input-div">
+            <label htmlFor="">Name</label>
+            <input type="text" nameRef={useRef} name="" id="" />
+          </div>
+          <div className="input-div">
+            <label htmlFor="">Age</label>
+            <input type="number" name="" id="" />
+          </div>
+          <div className="input-div">
+            <label htmlFor="">Place</label>
+            <input type="text" name="" id="" />
+          </div>
+          <button>Submit</button>
         </div>
       </div>
     </>
