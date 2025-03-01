fa52be46f895e29ddafb995e22521ef39d6ba211
BlesonPhilip <blesonphilip21@gmail.com>
2025-03-01 09:52:18 +0530

    ist enter key is done.

--- useref/src/App.jsx
+++ useref/src/App.jsx
@@ -4,17 +4,25 @@ import { useRef } from "react";
 
 const App = () => {
   const nameRef = useRef();
+  const ageRef = useRef();
+
+  const nameKey = (e) => {
+    if (e.key === "Enter") {
+      ageRef.current.focus();
+    }
+  };
+
   return (
     <>
       <div className="container">
         <div className="form">
           <div className="input-div">
             <label htmlFor="">Name</label>
-            <input type="text" nameRef={useRef} name="" id="" />
+            <input type="text" ref={nameRef} onKeyDown={nameKey} />
           </div>
           <div className="input-div">
             <label htmlFor="">Age</label>
-            <input type="number" name="" id="" />
+            <input type="number" ref={ageRef} />
           </div>
           <div className="input-div">
             <label htmlFor="">Place</label>
