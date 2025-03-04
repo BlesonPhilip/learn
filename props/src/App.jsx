d358cf81c70aaa23d4971309ad09d048383e5ae5
BlesonPhilip <blesonphilip21@gmail.com>
2025-03-04 10:28:01 +0530

    count  also added using useState

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import Card from "./components/card";
+import { useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const Increment = () => {
+    setCount(count + 1);
+  };
   return (
     <div>
+      <h1>{count}</h1>
+      <button onClick={Increment}>+</button>
       <Card title=" Title 1" content="hai how are you" />
       <Card title="Title 2" content="hey i am fine" />
       <Card title="Title 3" content="hey , what about you" />
